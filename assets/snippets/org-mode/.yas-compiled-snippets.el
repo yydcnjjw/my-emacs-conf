4d7cbe01a69d2f6127b5fc8bee8e6db59bc6c6cc
yydcnjjw <yydcnjjw@gmail.com>
2021-05-20 08:54:15 +0800

    update

---
+++ assets/snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("orgheader" "#+LATEX_HEADER: \\usepackage{xeCJK}\n#+LATEX_HEADER: \\setCJKmainfont{Noto Sans CJK SC}\n#+LATEX_HEADER: \\setCJKsansfont{Noto Serif CJK SC}\n#+LATEX_HEADER: \\setCJKmonofont{Noto Sans Mono CJK SC}\n#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"https://rgb-24bit.github.io/org-html-theme-list/org/style/main.css\"/>\n$0" "orgheader" nil nil nil "/home/yydcnjjw/.emacs.d/assets/snippets/org-mode/orgheader" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Jan 26 18:39:17 2021
