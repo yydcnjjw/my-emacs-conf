6f0f73a2484b5e4337a154ab6a6e2d5665dc8771
yydcnjjw <yydcnjjw@gmail.com>
2022-03-25 10:49:45 +0800

    refactor(wsl): clean code

--- core/my-wsl.el
+++ core/my-wsl.el
@@ -30,24 +30,19 @@
 (require 'my-const)
 
 (when my/wsl-p
-  (defun my/browse-url-generic (url &optional _new-window)
-    "Ask the WWW browser defined by `browse-url-generic-program' to load URL.
-Default to the URL around or before point.  A fresh copy of the
-browser is started up in a new process with possible additional arguments
-`browse-url-generic-args'.  This is appropriate for browsers which
-don't offer a form of remote control."
+  (defun my/browse-url-wsl (url &optional _new-window)
+    "browse url wsl"
     (interactive (browse-url-interactive-arg "URL: "))
-    (if (not browse-url-generic-program)
-        (error "No browser defined (`browse-url-generic-program')"))
-    (apply 'call-process browse-url-generic-program nil
-	       0 nil
-	       (append browse-url-generic-args
-                   (list (format "start %s"
-                                 (replace-regexp-in-string "&" "^&" url))))))
+    (if (not browse-url-wsl-program)
+        (error "No browser defined (`browse-url-wsl-program')"))
+    (apply #'start-process (concat "wsl-browse " url) nil
+           browse-url-wsl-program
+	       (append browse-url-wsl-args
+                   (list (format "start %s" url)))))
   (setq
-   browse-url-generic-program  "/mnt/c/Windows/System32/cmd.exe"
-   browse-url-generic-args     '("/c")
-   browse-url-browser-function #'my/browse-url-generic))
+   browse-url-wsl-program  "/mnt/c/Windows/System32/cmd.exe"
+   browse-url-wsl-args     '("/c")
+   browse-url-handlers '(("\\`https?:" . my/browse-url-wsl))))
 
 (provide 'my-wsl)
 
