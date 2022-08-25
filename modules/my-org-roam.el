bbea6352fd53a59d1d63c10d1101ce17e078f7c5
yydcnjjw <yydcnjjw@gmail.com>
2022-08-25 13:39:42 +0800

    feat(org-roam): add my/org-roam-backlinks-section
    group by backlinks based on id and outline

--- modules/my-org-roam.el
+++ modules/my-org-roam.el
@@ -44,7 +44,10 @@
    (org-roam-db-update-on-save t)
    (org-roam-node-display-template
     (concat "${title:*} "
-            (propertize "${tags:40}" 'face 'org-tag))))
+            (propertize "${tags:40}" 'face 'org-tag)))
+   (org-roam-mode-sections
+    '(my/org-roam-backlinks-section org-roam-reflinks-section))
+   )
   :bind (("C-c C-n /" . org-roam-node-find)
          ("C-c C-n r" . org-roam-db-sync)
          ("C-c C-n c" . org-roam-capture)
@@ -54,6 +57,30 @@
           ("C-c C-n a" . org-roam-alias-add)
           ("C-c C-n b" . org-roam-buffer-toggle)))
   :config
+  (defun my/org-roam-backlinks-groupby (backlink)
+    (cons
+     (org-roam-node-id (org-roam-backlink-source-node backlink))
+     (plist-get (org-roam-backlink-properties backlink) :outline)))
+  (defun my/org-roam-backlinks-map (groupby)
+    (car (cdr groupby)))
+  (cl-defun my/org-roam-backlinks-section (node)
+    "The backlinks section for NODE.
+
+When UNIQUE is nil, show all positions where references are found.
+When UNIQUE is t, limit to unique sources."
+    (when-let* ((backlinks (seq-sort #'org-roam-backlinks-sort
+                                     (seq-map #'my/org-roam-backlinks-map
+                                              (seq-group-by #'my/org-roam-backlinks-groupby
+                                                            (org-roam-backlinks-get node))))))
+      (magit-insert-section (org-roam-backlinks)
+        (magit-insert-heading "Backlinks:")
+        (dolist (backlink backlinks)
+          (org-roam-node-insert-section
+           :source-node (org-roam-backlink-source-node backlink)
+           :point (org-roam-backlink-point backlink)
+           :properties (org-roam-backlink-properties backlink)))
+        (insert ?\n))))
+
   (org-roam-db-autosync-mode))
 
 (use-package org-roam-ui
