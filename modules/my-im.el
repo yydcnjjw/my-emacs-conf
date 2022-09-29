2ee5739986edb9d4fb6486abd26731173e01f517
yydcnjjw <yydcnjjw@gmail.com>
2022-09-29 10:55:54 +0800

    feat(im): auto sync gtd

--- modules/my-im.el
+++ modules/my-im.el
@@ -29,15 +29,6 @@
 
 (require 'my-const)
 
-;; (use-package org-download
-;;   :defer t
-;;   :after org
-;;   :commands (org-download-clipboard)
-;;   :config
-;;   (setq org-download-image-dir "./image"
-;;         org-download-heading-lvl 1
-;;         ))
-
 (defcustom my/im-dir "~/workspace/my-im"
   "IM directory."
   :type 'directory
@@ -63,11 +54,26 @@
   :type 'file
   :group 'my)
 
+(defcustom my/agenda-sync-interval 60
+  "GTD agenda sync interval."
+  :type 'integer
+  :group 'my)
+
+(defcustom my/agenda-rclone-remote "gtd"
+  "GTD rclone remote."
+  :type 'string
+  :group 'my)
+
+(defcustom my/agenda-rclone-remote-gtd-dir "gtd"
+  "GTD rclone remote directory."
+  :type 'string
+  :group 'my)
+
 (use-package org
   :defer t
   :bind
-  ("C-c c" . org-capture)
-  ("C-c a" . org-agenda))
+  (("C-c c" . org-capture)
+   ("C-c a" . org-agenda)))
 
 (use-package org-capture
   :straight nil
@@ -133,7 +139,55 @@
      ("p" "project group view"
       ((alltodo "" ((org-agenda-files (list my/agenda-project-file))
                     (org-super-agenda-groups
-                     '((:auto-parent))))))))))
+                     '((:auto-parent)))))))))
+
+  :init
+  (defvar my/agenda-sync-buffer-name "*agenda-sync*")
+  (defvar my/agenda-sync-timer nil)
+
+  (defun my/agenda-sync-cmd ()
+    (format "rclone sync -P %s %s:%s" my/gtd-dir my/agenda-rclone-remote my/agenda-rclone-remote-gtd-dir))
+
+  (defun my/run-agenda-sync-timer ()
+    (unless my/agenda-sync-timer
+      (message (format "Starting automatically sync for %s" my/gtd-dir))
+      (setq my/agenda-sync-timer
+            (run-with-timer
+             my/agenda-sync-interval nil
+             (lambda ()
+               (cancel-timer my/agenda-sync-timer)
+               (setq my/agenda-sync-timer nil)
+               (my/async-org-agenda-sync)
+               )))))
+
+  (defun my/agenda-sync-sentinel (proc event)
+    (my/run-agenda-sync-timer))
+  
+  (defun my/async-agenda-sync ()
+    (interactive)
+    (let ((proc (start-process-shell-command my/agenda-sync-buffer-name
+                                             my/agenda-sync-buffer-name
+                                             (my/agenda-sync-cmd))))
+      (set-process-sentinel proc #'my/org-agenda-sync-sentinel)))
+
+  (defun my/agenda-sync ()
+    (interactive)
+    (message (format "Agenda: sync %s" my/gtd-dir))
+    (call-process-shell-command (my/agenda-sync-cmd)
+                                            nil
+                                            my/agenda-sync-buffer-name
+                                            t))
+
+  (defun my/agenda-sync-after-save-hook-func ()
+    (when (and (eq major-mode 'org-mode)
+               (string-prefix-p my/gtd-dir buffer-file-name))
+      (my/agenda-sync)))
+
+  (when (daemonp)
+    (my/run-agenda-sync-timer))
+
+  :hook
+  (after-save . my/agenda-sync-after-save-hook-func))
 
 (my/require-modules
  '(org-roam
@@ -143,6 +197,15 @@
    ;; org-fc
    )
  )
+
+;; (use-package org-download
+;;   :defer t
+;;   :after org
+;;   :commands (org-download-clipboard)
+;;   :config
+;;   (setq org-download-image-dir "./image"
+;;         org-download-heading-lvl 1
+;;         ))
 
 (provide 'my-im)
 
