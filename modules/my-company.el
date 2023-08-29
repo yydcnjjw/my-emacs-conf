4c9ca68ae92b17bc37b89d674d5862a73ce2b80c
yydcnjjw <yydcnjjw@gmail.com>
2023-08-29 14:35:40 +0800

    feat(company): remove company-tabnine

--- modules/my-company.el
+++ modules/my-company.el
@@ -27,39 +27,6 @@
 
 ;;; Code:
 
-(use-package company-tabnine
-  :after company
-  :config
-  (defun my/company/sort-by-tabnine (candidates)
-    (if (or (functionp company-backend)
-            (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
-        candidates
-      (let ((candidates-table (make-hash-table :test #'equal))
-            candidates-1
-            candidates-2)
-        (dolist (candidate candidates)
-          (if (eq (get-text-property 0 'company-backend candidate)
-                  'company-tabnine)
-              (unless (gethash candidate candidates-table)
-                (push candidate candidates-2))
-            (push candidate candidates-1)
-            (puthash candidate t candidates-table)))
-        (setq candidates-1 (nreverse candidates-1))
-        (setq candidates-2 (nreverse candidates-2))
-        (nconc (seq-take candidates-1 2)
-               (seq-take candidates-2 2)
-               (seq-drop candidates-1 2)
-               (seq-drop candidates-2 2)))))
-
-  (add-to-list 'company-transformers 'my/company/sort-by-tabnine t)
-
-  (defun my/tabnine-installed-p ()
-    (condition-case nil
-        (company-tabnine--executable-path)
-      (error nil))
-    )
-  )
-
 (use-package company
   :defer t
   :hook
@@ -75,9 +42,6 @@
           company-minimum-prefix-length 1
           company-backends `(company-cmake
                              company-files
-                             ;; ,(if (my/tabnine-installed-p)
-                             ;;      '(company-capf :with company-tabnine :separate)
-                             ;;    'company-capf)
                              company-capf
                              company-keywords
                              company-dabbrev)))
