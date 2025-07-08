fa428bee304ec447b1f51cd809bcca57a33a5e9a
yydcnjjw <yydcnjjw@gmail.com>
2025-07-08 20:39:37 +0800

    fix: Use HTTP/HTTPS proxies consistently

--- core/my-proxy.el
+++ core/my-proxy.el
@@ -29,22 +29,21 @@
 
 (require 'my-const)
 
+;; (defconst my/proxy-address "127.0.0.1")
+;; ;; (defconst my/proxy-address "172.23.224.1")
+;; (defconst my/http-proxy-port "8118")
+;; (defconst my/socks5-proxy-port "8888")
 
-(defconst my/proxy-address (if my/wsl-p
-                               my/wsl-host-address
-                             "127.0.0.1"))
+;; (when (or (not (boundp 'url-proxy-services)) (null url-proxy-services))
+;;   (setq url-proxy-services
+;;           `(("http" . ,(format "%s:%s" my/proxy-address my/http-proxy-port))
+;;             ("https" . ,(format "%s:%s" my/proxy-address my/http-proxy-port))
+;;             )))
 
-(defconst my/http-proxy-port "8118")
-(defconst my/socks5-proxy-port "8888")
-
-(when (or (not (boundp 'url-proxy-services)) (null url-proxy-services))
-  (setq url-proxy-services
-          `(("http" . ,(format "%s:%s" my/proxy-address my/http-proxy-port))
-            ("https" . ,(format "%s:%s" my/proxy-address my/http-proxy-port))
-            )))
-
-;; (setenv "HTTP_PROXY" (format "socks5://%s:%s" my/proxy-address my/socks5-proxy-port))
-;; (setenv "HTTPS_PROXY" (format "socks5://%s:%s" my/proxy-address my/socks5-proxy-port))
+;; (setenv "HTTP_PROXY" (format "http://%s:%s" my/proxy-address my/http-proxy-port))
+;; (setenv "HTTPS_PROXY" (format "https://%s:%s" my/proxy-address my/http-proxy-port))
+;; (setenv "http_proxy" (format "http://%s:%s" my/proxy-address my/http-proxy-port))
+;; (setenv "https_proxy" (format "https://%s:%s" my/proxy-address my/http-proxy-port))
 
 (provide 'my-proxy)
 
