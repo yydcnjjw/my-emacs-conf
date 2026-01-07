11b185f4e355d47f75f8affd99d8b506b09c93aa
yydcnjjw <yydcnjjw@gmail.com>
2026-01-07 09:03:59 +0800

    fix(email): update mu4e source and disable mu4e-views
    - Change mu4e source from djcb/mu to emacsmirror/mu4e
    - Update mu4e-mu-binary path to use new repo directory
    - Comment out mu4e-views package configuration

--- config/init-email.el
+++ config/init-email.el
@@ -32,14 +32,14 @@
 
 (use-package mu4e
   :straight (:host github
-                   :repo "djcb/mu"
+                   :repo "emacsmirror/mu4e"
                    :branch "master"
                    :files ("build/mu4e/*")
                    :pre-build (("./autogen.sh")
                                ("make")))
   :config
   (setopt mail-user-agent 'mu4e-user-agent
-          mu4e-mu-binary (expand-file-name "build/mu/mu" (straight--repos-dir "mu"))
+          mu4e-mu-binary (expand-file-name "build/mu/mu" (straight--repos-dir "mu4e"))
           mu4e-sent-messages-behavior 'sent
           mu4e-sent-folder "/Sent"
           mu4e-drafts-folder "/Drafts"
@@ -56,38 +56,37 @@
   :bind
   (("C-c m" . mu4e)))
 
-(require 'my-frame)
-
-(use-package mu4e-views
-  :after mu4e
-  :straight (mu4e-views
-             :type git
-             :host github
-             :repo "lordpretzel/mu4e-views")
-  :bind (:map mu4e-headers-mode-map
-              ("v" . mu4e-views-mu4e-select-view-msg-method) ;; select viewing method
-              ("M-n" . mu4e-views-cursor-msg-view-window-down) ;; from headers window scroll the email view
-              ("M-p" . mu4e-views-cursor-msg-view-window-up) ;; from headers window scroll the email view
-              ("f" . mu4e-views-toggle-auto-view-selected-message) ;; toggle opening messages automatically when moving in the headers view
-              ("i" . mu4e-views-mu4e-view-as-nonblocked-html) ;; show currently selected email with all remote content
-              )
-  :init
-  (defun my/mu4e-views-graphic ()
-    (setq mu4e-views-default-view-method "html")
-    (mu4e-views-mu4e-use-view-msg-method "html"))
+;; (require 'my-frame)
+;; (use-package mu4e-views
+;;   :after mu4e
+;;   :straight (mu4e-views
+;;              :type git
+;;              :host github
+;;              :repo "lordpretzel/mu4e-views")
+;;   :bind (:map mu4e-headers-mode-map
+;;               ("v" . mu4e-views-mu4e-select-view-msg-method) ;; select viewing method
+;;               ("M-n" . mu4e-views-cursor-msg-view-window-down) ;; from headers window scroll the email view
+;;               ("M-p" . mu4e-views-cursor-msg-view-window-up) ;; from headers window scroll the email view
+;;               ("f" . mu4e-views-toggle-auto-view-selected-message) ;; toggle opening messages automatically when moving in the headers view
+;;               ("i" . mu4e-views-mu4e-view-as-nonblocked-html) ;; show currently selected email with all remote content
+;;               )
+;;   :init
+;;   (defun my/mu4e-views-graphic ()
+;;     (setq mu4e-views-default-view-method "html")
+;;     (mu4e-views-mu4e-use-view-msg-method "html"))
   
-  (setopt mu4e-views-completion-method 'default
-          mu4e-views-next-previous-message-behaviour 'stick-to-current-window
-          mu4e-views-auto-view-selected-message t
-          mu4e-views-html-filter-external-content nil)
-  (when (display-graphic-p)
-    (my/mu4e-views-graphic)))
+;;   (setopt mu4e-views-completion-method 'default
+;;           mu4e-views-next-previous-message-behaviour 'stick-to-current-window
+;;           mu4e-views-auto-view-selected-message t
+;;           mu4e-views-html-filter-external-content nil)
+;;   (when (display-graphic-p)
+;;     (my/mu4e-views-graphic)))
 
 (use-package mu4e-alert
   :after mu4e
   :commands
   (mu4e-alert-enable-notifications)
-  :config
+  :init
   (mu4e-alert-enable-notifications))
 
 (provide 'init-email)
